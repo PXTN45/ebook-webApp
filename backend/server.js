90e8c82aeb2b5c1babe168511be3f0a61484da9f
PXTN2 <150564535+PXTN2@users.noreply.github.com>
2024-03-27 12:56:11 +0700

    connect mongoDB and server

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const router = express.Router();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+//creat server
+const app = express();
+const CLIENT_URL = process.env.CLIENT_URL;
+app.use(cors({ credentials: true, origin: CLIENT_URL }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//connect mongoDB
+const MONGODB_URL = process.env.MONGODB_URL;
+mongoose.connect(MONGODB_URL);
+
+app.get("/", (req, res) => {
+  res.send("<h1> Welcome to H2O Project</h1>");
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log("Server is running on http://localhost:" + PORT);
+});
